7bea0ed605432405b1a4470c1e4d943698993c22
patty23418 <130178966+patty23418@users.noreply.github.com>
2024-10-30 16:47:35 -0500

    Update SettingsPage.cs

--- SettingsPage.cs
+++ SettingsPage.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +15,41 @@ namespace CSCI363Project
         public SettingsPage()
         {
             InitializeComponent();
+        }
+
+        private void updateAppBox_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Application has restarted");
+        }
+
+        private void dayTimeBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nightTimeBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mainBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void carInfoBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void controlsBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void settingsBox_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
